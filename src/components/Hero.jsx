98973554051872e261ec067a101da8e975755dd3
Rob Sassack <rsassack25@gmail.com>
2022-10-28 19:24:47 -0400

    Added hero component with styling

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero--gallery-container">
+        <img src="gallery.png" alt="Experiences image gallery" className='hero--gallery'/>
+      </div>
+      <div className="hero--text-container">
+        <h1 className='hero--title'>Online Experiences</h1>
+        <p className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
