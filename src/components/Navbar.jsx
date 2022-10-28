34c975ab3e713c9284a2eb27bbfa52dc4b561ace
Rob Sassack <rsassack25@gmail.com>
2022-10-27 21:26:18 -0400

    Added navbar with styling

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="logo.png" alt="Logo" className="navbar--logo"/>
+    </nav>
+  );
+}
+
+export default Navbar;
